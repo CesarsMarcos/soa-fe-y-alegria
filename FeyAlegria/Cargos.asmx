cd7c948dc687a3be01cfee90727050a7829d5a6e
César Marcos <39070231+CesarsMarcos@users.noreply.github.com>
2024-12-02 22:03:31 -0500

    feat: agregar servicio soa para cargos

---
+++ FeyAlegria/Cargos.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="Cargos.asmx.cs" Class="FeyAlegria.Cargo" %>
---
+++ FeyAlegria/Cargos.asmx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace FeyAlegria
+{
+    /// <summary>
+    /// Descripción breve de Cargo
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Cargos : System.Web.Services.WebService
+    {
+
+        private readonly string connectionString =
+           "Server=DESKTOP-DISQQ64;Initial Catalog=db_fe_y_alegria;Integrated Security=True;";
+
+
+        [WebMethod]
+        public DataSet listado()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                using (SqlDataAdapter da = new SqlDataAdapter(
+                    @"SELECT [id_cargo] ,[nombre] ,[descripcion] ,[fecha_registro] ,[estado] FROM [dbo].[tb_cargos] WHERE estado=1", con))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds);
+                    return ds;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al listar cursos: " + ex.Message);
+            }
+        }
+
+        [WebMethod]
+        public String registrar(
+            String nombre,
+            String descripcion)
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(
+                     @"INSERT INTO [dbo].[tb_cargos]
+                           ([nombre]
+                           ,[descripcion]
+                           ,[fecha_registro]
+                           ,[estado])
+                     VALUES
+                           (@nombre
+                           ,@descripcion
+                           ,@fecha_registro
+                           ,@estado)", con))
+                {
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@descripcion", descripcion);
+                    cmd.Parameters.AddWithValue("@fecha_registro", DateTime.Now);
+                    cmd.Parameters.AddWithValue("@estado", true);
+                    con.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    return rowsAffected > 0 ? "Cargo registrada correctamente." : "No se pudo registrar el Cargo.";
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                return "Error SQL: " + sqlEx.Message;
+            }
+            catch (Exception ex)
+            {
+                return "Error general: " + ex.Message;
+            }
+        }
+
+    }
+}
